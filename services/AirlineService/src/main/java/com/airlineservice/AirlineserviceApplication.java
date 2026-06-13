079c8adb4aa76fd910616237b567ddead1b44ec0
ankit-2210 <78948475+ankit-2210@users.noreply.github.com>
2026-06-13 15:34:54 +0530

    added airline service

---
+++ services/AirlineService/src/main/java/com/airlineservice/AirlineserviceApplication.java
@@ -0,0 +1,13 @@
+package com.airlineservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineserviceApplication.class, args);
+	}
+
+}
---
+++ services/AirlineService/src/test/java/com/airlineservice/AirlineserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airlineservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
